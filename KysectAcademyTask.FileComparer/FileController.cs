f19daa8f9b0e814271afcc031d3f83987d7963aa
&blueblood <khashchuk.denis@gmail.com>
2022-07-11 00:24:40 +0300

    chore: project init + parsing tries
    GroupDocs.Comparison doesn't work as i want to, so comparing algo must be changed

---
+++ KysectAcademyTask.FileComparer/FileController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using GroupDocs.Comparison;
+namespace KysectAcademyTask.FileComparer
+{
+    public class FileController
+    {
+        public  string? InputPath { get; set; }
+        public  string? OutputPath { get; set; }
+        
+        public FileController(){}
+        
+        public FileController(string inputPath, string outputPath)
+         {
+             ArgumentNullException.ThrowIfNull(inputPath);
+             ArgumentNullException.ThrowIfNull(outputPath);
+             string[] files = Directory.GetFiles(InputPath ?? string.Empty);
+             if (files.Length < 2)
+                 throw new ArgumentException("There are less than two files in input directory");
+             InputPath = inputPath;
+             OutputPath = outputPath;
+         }
+         public void CompareFiles()//change algo to damerau-levenshtein distance
+        {
+            string[] files = Directory.GetFiles(InputPath ?? string.Empty);
+            
+            for (int i = 0; i < files.Length; i++)
+            {
+                for (int j = i+1; j < files.Length; j++)
+                {
+                    Comparer comparer = new Comparer(File.OpenRead(files[i]));
+                    comparer.Add(File.OpenRead(files[j]));
+                    comparer.Compare(OutputPath);
+                }
+            }
+        }
+    }
+}
---
+++ KysectAcademyTask.FileComparer/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace KysectAcademyTask.FileComparer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConfigurationRoot? config = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json").Build(); 
+            FileController? pathKeeperConfig = config.GetSection(nameof(FileController)).Get<FileController>();
+            pathKeeperConfig?.CompareFiles();
+        }
+    }
+}
