3b30222ff787c5dd542ccc90d130c851727d66ae
Fredi Kats <fredikats@outlook.com>
2022-07-22 22:24:50 +0400

    fix: disable NRT

--- KysectAcademyTask.FileComparer/Comparator.cs
+++ KysectAcademyTask.FileComparer/Comparator.cs
@@ -1,15 +1,11 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-
-namespace KysectAcademyTask.FileComparer
+﻿namespace KysectAcademyTask.FileComparer
 {
     public class Comparator : IComparator
     {
         public double Compare(string sourceFile, string targetFile)
         {
             string[] linesInSource = File.ReadAllLines(sourceFile);
-            IEnumerable<string>? difference = linesInSource.Except(File.ReadAllLines(targetFile));
+            IEnumerable<string> difference = linesInSource.Except(File.ReadAllLines(targetFile));
             return (double) (linesInSource.Count() - difference.Count()) / linesInSource.Length * 100;
         }
     }
