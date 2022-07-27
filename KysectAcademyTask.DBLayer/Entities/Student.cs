01d7da50ac1e3606619d4a64f9930787dc731f3d
&blueblood <khashchuk.denis@gmail.com>
2022-07-27 21:39:35 +0300

    chore:project init
    first tries in EF

---
+++ KysectAcademyTask.DBLayer/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace KysectAcademyTask.DBLayer
+{
+
+    public class Student
+    {
+        public int Id { get; }
+        public string Name { get;  }
+        public string GroupName { get;  }
+        
+        
+
+        public Student(string name, string groupName, int id)
+        {
+            Name = name;
+            GroupName = groupName;
+            Id = id;
+        }
+    }
+}
---
+++ KysectAcademyTask.DBLayer/Entities/Submission.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KysectAcademyTask.DBLayer
+{
+    public class Submission
+    {
+        public int Id { get; }
+        [ForeignKey("Student")] public int StudentSubmissionId { get; }
+        private string HomeworkName { get; }
+
+        public Submission(int id, int studentSubmissionId, string homeworkName)
+        {
+            Id = id;
+            StudentSubmissionId = studentSubmissionId;
+            HomeworkName = homeworkName;
+        }
+    }
+}
