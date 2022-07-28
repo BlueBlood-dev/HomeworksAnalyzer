34c9dfa19edd6131fe4b8f72719b7ff93d3337b9
&blueblood <khashchuk.denis@gmail.com>
2022-07-28 18:09:06 +0300

    fix: delete temporary projects

--- KysectAcademyTask.FileComparer/Program.cs
+++ KysectAcademyTask.FileComparer/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-             new DirectoryConfigReader().Read().CompareFiles();
+            new DirectoryConfigReader().Read().CompareFiles();
         }
     }
 }
