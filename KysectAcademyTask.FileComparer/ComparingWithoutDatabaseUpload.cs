72616a40c88c5a78fbe0aa3b9206853b0d415eb0
&blueblood <khashchuk.denis@gmail.com>
2022-08-07 21:21:32 +0300

    Update ComparingWithoutDatabaseUpload.cs
    temp data deleted

--- KysectAcademyTask.FileComparer/ComparingWithoutDatabaseUpload.cs
+++ KysectAcademyTask.FileComparer/ComparingWithoutDatabaseUpload.cs
@@ -1,55 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using KysectAcademyTask.DatabaseLayer;
-using KysectAcademyTask.DatabaseLayer.Entities;
-using KysectAcademyTask.FileComparer.Comparators;
 using KysectAcademyTask.FileComparer.Interfaces;
 using KysectAcademyTask.FileComparer.Models;
-using Microsoft.EntityFrameworkCore;
+
 
 
 namespace KysectAcademyTask.FileComparer
 {
     public class ComparingWithoutDatabaseUpload : ISubmitsComparerLogic
     {
-
-
-        private int FindSubmitIdAndAddResult(double result, Submit whSubmit, Submit submit)
-        {
-            using DataBaseContext db = new DataBaseBuilder().Build();
-            List<Student> students = db.Student.Include(s => s.Submissions).ToList();
-            throw new NotImplementedException();
-        }
-    
-        
-        
-        
-        
         public void ComparingProcess(List<Submit> submits, List<Submit> whiteSubmits, IComparator comparator,
             IWriter writer, string outputPath, string inputPath)
         {
-            int amountOfCmp = submits.Count * whiteSubmits.Count;
-            int counter = 1;
-            SubmitsComparer comparer = new();
-            DirectorySubmitPathGetter getter = new();
-
-            foreach (Submit whSubmit in whiteSubmits)
-            {
-                foreach (Submit submit in submits)
-                {
-                    if (whSubmit.HomeworkName.Equals(submit.HomeworkName) &&
-                        !whSubmit.StudentName.Equals(submit.StudentName))
-                    {
-                        double compareSubmitsResult = comparer.CompareSubmits(whSubmit, submit,
-                            new(getter.GetSubmitPath(whSubmit, inputPath)),
-                            new DirectoryInfo(getter.GetSubmitPath(submit, inputPath)), writer, comparator, outputPath);
-                    }
-
-                    Console.WriteLine($"{counter++}\\{amountOfCmp}");
-                }
-            }
+            throw new NotImplementedException();
         }
     }
 }
