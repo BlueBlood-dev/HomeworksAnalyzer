019ee5022a7fd73d1804fc31222a40b406a76b5e
&blueblood <khashchuk.denis@gmail.com>
2022-08-09 21:25:18 +0300

    refactor: extra braces removed

--- KysectAcademyTask.FileComparer/ComparingWithDatabaseUpload.cs
+++ KysectAcademyTask.FileComparer/ComparingWithDatabaseUpload.cs
@@ -7,7 +7,6 @@ using KysectAcademyTask.DatabaseLayer.Entities;
 using KysectAcademyTask.FileComparer.Comparators;
 using KysectAcademyTask.FileComparer.Interfaces;
 using KysectAcademyTask.FileComparer.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace KysectAcademyTask.FileComparer
 {
@@ -20,22 +19,21 @@ namespace KysectAcademyTask.FileComparer
             using DataBaseContext db = new DataBaseBuilder().Build();
             var dataBaseInitializer = new DataBaseInitializer();
             ICollection<ResultOfCompare> resultOfCompares = new List<ResultOfCompare>();
-             if (db.Student.ToList().Count == 0 || db.Submissions.ToList().Count == 0)
-             {
-                 dataBaseInitializer.Initialize(submits,db);
-             }
-             
-             foreach (Submit whiteSubmit in whiteSubmits)
+            if (db.Student.ToList().Count == 0 || db.Submissions.ToList().Count == 0)
+                dataBaseInitializer.Initialize(submits, db);
+
+
+            foreach (Submit whiteSubmit in whiteSubmits)
             {
                 foreach (Submit submit in submits)
                 {
                     int firstStudentId = dataBaseInitializer.FindStudentId(whiteSubmit.StudentName, db);
                     int secondStudentId = dataBaseInitializer.FindStudentId(whiteSubmit.StudentName, db);
-                    
-                    int firstSubmissionId = dataBaseInitializer.FindSubmissionId(firstStudentId,db);
-                    int secondSubmissionId = dataBaseInitializer.FindSubmissionId(secondStudentId,db);
 
-                    if (!new UniqueValueController().CheckIfResultExists(firstSubmissionId, secondSubmissionId,db) &&
+                    int firstSubmissionId = dataBaseInitializer.FindSubmissionId(firstStudentId, db);
+                    int secondSubmissionId = dataBaseInitializer.FindSubmissionId(secondStudentId, db);
+
+                    if (!new UniqueValueController().CheckIfResultExists(firstSubmissionId, secondSubmissionId, db) &&
                         submit.HomeworkName == whiteSubmit.HomeworkName &&
                         submit.StudentName != whiteSubmit.StudentName)
                     {
@@ -48,6 +46,7 @@ namespace KysectAcademyTask.FileComparer
                     }
                 }
             }
+
             Console.WriteLine("SAVING THE RESULT");
             new DataBaseSaver().Save(resultOfCompares, db);
         }
